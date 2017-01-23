2826d710151cf04f98ce95036999f08a9db722ac
neimad971 <damientalbot26@gmail.com>
2017-01-23 19:24:14 +0000

    bugfix on indexes for draw_vertical_segment and draw_horizontal_segment methods

--- app/bitmap.rb
+++ app/bitmap.rb
@@ -31,10 +31,10 @@ class Bitmap
   	def draw_vertical_line(in_x, from_y1, to_y2, color)
   	  if in_x >= 1 && in_x <= @columns && from_y1 >= 1 && from_y1 <= @rows && to_y2 >= 1 && to_y2 <= @rows
   	    @array.each_with_index do |row, row_index|        
-          if(row_index >= from_y1 && row_index <= to_y2)
+          if(row_index + 1 >= from_y1 && row_index + 1 <= to_y2)
             row.each_with_index do |col, col_index|
-              if(col_index == in_x)
-                @array[row_index - 1][col_index -1] = color
+              if(col_index + 1 == in_x)
+                @array[row_index][col_index] = color
               end
             end
           end
@@ -48,10 +48,10 @@ class Bitmap
   	def draw_horizontal_line(in_y, from_x1, to_x2, color)
   	  if in_y >= 1 && in_y <= @rows && from_x1 >= 1 && from_x1 <= @columns && to_x2 >= 1 && to_x2 <= @columns
   	    @array.each_with_index do |row, row_index|        
-          if(row_index == in_y)
+          if(row_index + 1 == in_y)
             row.each_with_index do |col, col_index|
-              if(col_index >= from_x1 && col_index <= to_x2)
-                @array[row_index -1][col_index -1] = color
+              if(col_index + 1 >= from_x1 && col_index + 1 <= to_x2)
+                @array[row_index][col_index] = color
               end
             end
           end
@@ -69,5 +69,4 @@ class Bitmap
       	puts "Try again : X should be between 1 and #{@columns}; Y should be between 1 and #{@rows} 😉"
       end
     end
-
 end
